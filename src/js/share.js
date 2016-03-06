6544e761cd67b6b567c4ca9d480cf5937d7192b4
nicholasruggeri <ruggerinicholas@gmail.com>
2016-03-06 21:49:12 +0100

    hotfix share.js

--- src/js/share.js
+++ src/js/share.js
@@ -9,10 +9,18 @@ export default class Sharer {
   }
 
   shareTW(url, name, images) {
+
+    let pos = {
+      left: screen.width / 2 - 600 / 2,
+      top: screen.height / 2 - 260 / 2
+    };
+
+    console.log('pos', pos)
+
     window.open(
-      `https://twitter.com/share?url=${escape(url)}&text=I'm listing ${name} playlist, via @WeatheryMood`,
+      `https://twitter.com/share?url=${escape(url)}&text=I'm listing "${name}" playlist, via @WeatheryMood`,
       "Share playlist via @WeatheryMood @spotify",
-      "menubar=no,toolbar=no,resizable=yes,height=253,width=600",
+      `menubar=no,toolbar=no,resizable=yes,height=253,width=600,left=${pos.left},top=${pos.top}`,
     )
     return false;
   }
