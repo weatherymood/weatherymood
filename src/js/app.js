618736afd0410122327e0c6b811cb3ca98634f14
nicholasruggeri <ruggerinicholas@gmail.com>
2016-03-07 15:22:00 +0100

    refactoring app.js and remove iframe option

--- src/js/app.js
+++ src/js/app.js
@@ -18,33 +18,37 @@ let Share  = new share()
 let firstCall, secondCall, thirdCall
 let d = document
 
-let isIframe = false
-
 let loadImg = (src, callback) => {
-
     let sprite = new Image()
     sprite.onload = callback
     sprite.src = src
+}
 
+let checkPosition = (resolve, reject) => {
+    Geolocation.checkPosition(() => {
+        resolve()
+        setTimeout(()=>{
+            Preloader.remove()
+        }, 0)
+    })
+}
+
+let getWeather = (resolve, reject) => {
+    Weather.getWeather(function (data) {
+        resolve(data)
+    })
 }
 
 if (navigator.onLine){
-    firstCall = new Promise(function (resolve, reject) {
+    firstCall = new Promise((resolve, reject) => {
 
-        Geolocation.checkPosition(function(){
-            resolve()
-            setTimeout(()=>{
-                Preloader.remove()
-            }, 0)
-        })
+        checkPosition(resolve, reject)
 
     }).then(function(data){
 
         secondCall = new Promise(function (resolve, reject) {
 
-            Weather.getWeather(function (data) {
-                resolve(data)
-            })
+            getWeather(resolve, reject)
 
         }).then(function (data) {
 
@@ -65,56 +69,40 @@ if (navigator.onLine){
                     }
                 })
 
-                if (isIframe) {
-                    let track = {
-                        uri: data.uri,
-                    }
-                    let iframe = `<iframe id="card" src="https://embed.spotify.com/?uri=${track.uri}&view=coverart" width="300" height="380" frameborder="0" allowtransparency="true"></iframe>`
-
-                    d.getElementById("song-cover").innerHTML = iframe
-
-                    d.getElementById("card").addEventListener('load', function() {
-                        d.getElementById("card").className += "flipped"
-                        d.getElementById("song-details").className += "active"
-                        Background.setBackground(Moods.getClass(mood))
-                    })
-                } else {
-
-                    let track = {
-                        uri: data.uri,
-                        name: data.name,
-                        cover: data.images[0].url
-                    }
-
-                    loadImg(track.cover, function() {
-
-                        d.getElementById("song-cover")
-                         .insertAdjacentHTML('beforeend',`
-                            <div id="card">
-                                <img src="${track.cover}">
-                                <div id="song-play">
-                                    <a href="${track.uri}" class="play"></a>
-                                    <div id="song-replay">
-                                        <svg class="svg-icon" viewBox="0 0 20 20">
-                                            <path fill="#fff" d="M3.254,6.572c0.008,0.072,0.048,0.123,0.082,0.187c0.036,0.07,0.06,0.137,0.12,0.187C3.47,6.957,3.47,6.978,3.484,6.988c0.048,0.034,0.108,0.018,0.162,0.035c0.057,0.019,0.1,0.066,0.164,0.066c0.004,0,0.01,0,0.015,0l2.934-0.074c0.317-0.007,0.568-0.271,0.56-0.589C7.311,6.113,7.055,5.865,6.744,5.865c-0.005,0-0.01,0-0.015,0L5.074,5.907c2.146-2.118,5.604-2.634,7.971-1.007c2.775,1.912,3.48,5.726,1.57,8.501c-1.912,2.781-5.729,3.486-8.507,1.572c-0.259-0.18-0.618-0.119-0.799,0.146c-0.18,0.262-0.114,0.621,0.148,0.801c1.254,0.863,2.687,1.279,4.106,1.279c2.313,0,4.591-1.1,6.001-3.146c2.268-3.297,1.432-7.829-1.867-10.101c-2.781-1.913-6.816-1.36-9.351,1.058L4.309,3.567C4.303,3.252,4.036,3.069,3.72,3.007C3.402,3.015,3.151,3.279,3.16,3.597l0.075,2.932C3.234,6.547,3.251,6.556,3.254,6.572z"></path>
-                                        </svg>
-                                    </div>
-                                </div>
-                            `);
-
-                        d.getElementById("song-name").innerHTML = track.name
-
-                        setTimeout(function() {
-                            d.getElementById("card").className += "flipped"
-                        }, 100)
-
-                        setTimeout(function () {
-                            Background.setBackground(Moods.getClass(mood))
-                            d.getElementById("song-details").className += "active"
-                        }, 500)
-
-                    })
+                let track = {
+                    uri: data.uri,
+                    name: data.name,
+                    cover: data.images[0].url
                 }
+
+                loadImg(track.cover, function() {
+
+                    d.getElementById("song-cover")
+                     .insertAdjacentHTML('beforeend',`
+                        <div id="card">
+                            <img src="${track.cover}">
+                            <div id="song-play">
+                                <a href="${track.uri}" class="play"></a>
+                                <div id="song-replay">
+                                    <svg class="svg-icon" viewBox="0 0 20 20">
+                                        <path fill="#fff" d="M3.254,6.572c0.008,0.072,0.048,0.123,0.082,0.187c0.036,0.07,0.06,0.137,0.12,0.187C3.47,6.957,3.47,6.978,3.484,6.988c0.048,0.034,0.108,0.018,0.162,0.035c0.057,0.019,0.1,0.066,0.164,0.066c0.004,0,0.01,0,0.015,0l2.934-0.074c0.317-0.007,0.568-0.271,0.56-0.589C7.311,6.113,7.055,5.865,6.744,5.865c-0.005,0-0.01,0-0.015,0L5.074,5.907c2.146-2.118,5.604-2.634,7.971-1.007c2.775,1.912,3.48,5.726,1.57,8.501c-1.912,2.781-5.729,3.486-8.507,1.572c-0.259-0.18-0.618-0.119-0.799,0.146c-0.18,0.262-0.114,0.621,0.148,0.801c1.254,0.863,2.687,1.279,4.106,1.279c2.313,0,4.591-1.1,6.001-3.146c2.268-3.297,1.432-7.829-1.867-10.101c-2.781-1.913-6.816-1.36-9.351,1.058L4.309,3.567C4.303,3.252,4.036,3.069,3.72,3.007C3.402,3.015,3.151,3.279,3.16,3.597l0.075,2.932C3.234,6.547,3.251,6.556,3.254,6.572z"></path>
+                                    </svg>
+                                </div>
+                            </div>
+                        `);
+
+                    d.getElementById("song-name").innerHTML = track.name
+
+                    setTimeout(function() {
+                        d.getElementById("card").className += "flipped"
+                    }, 100)
+
+                    setTimeout(function () {
+                        Background.setBackground(Moods.getClass(mood))
+                        d.getElementById("song-details").className += "active"
+                    }, 500)
+
+                })
 
             }).catch(function (response) {
                 console.log('err', response)
